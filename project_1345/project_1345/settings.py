f6d4f88773c134f5f1cf7b8f65a5b396fdbcb933
lexxai <lexxai@gmail.com>
2025-01-16 18:55:10 +0200

    INSTALLED_APPS, CART_SESSION_ID

--- project_1345/project_1345/settings.py
+++ project_1345/project_1345/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'drf_yasg',
+    "drf_yasg",
+    "cart.apps.CartConfig",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CART_SESSION_ID = "cart"
