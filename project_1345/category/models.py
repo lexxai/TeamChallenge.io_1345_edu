3d2c9f65f43fdf962373bb42b092999301d4b0ca
lexxai <lexxai@gmail.com>
2025-01-16 19:27:07 +0200

    make migrations

--- project_1345/category/models.py
+++ project_1345/category/models.py
@@ -10,6 +10,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class CategorySchema(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     schema = models.JSONField()
