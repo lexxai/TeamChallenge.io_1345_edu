386b1733a4a37abd727f0802d975d3ff14db3d96
lexxai <lexxai@gmail.com>
2025-01-16 19:04:18 +0200

    SESSION_COOKIE_AGE, INSTALLED_APPS, class Cart

---
+++ project_1345/cart/cart.py
@@ -0,0 +1,49 @@
+from decimal import Decimal
+
+from django.conf import settings
+
+from project_1345.cart.models import Product
+
+
+class Cart(object):
+    def __init__(self, request):
+        self.session = request.session
+        cart = self.session.get(settings.CART_SESSION_ID)
+        if not cart:
+            # save an empty cart in the session
+            cart = self.session[settings.CART_SESSION_ID] = {}
+        self.cart = cart
+
+    def add(self, product, quantity=1, override_quantity=False):
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {"quantity": 0, "price": str(product.price)}
+        if override_quantity:
+            self.cart[product_id]["quantity"] = quantity
+        else:
+            self.cart[product_id]["quantity"] += quantity
+        self.save()
+
+    def save(self):
+        # update the session cart
+        self.session[settings.CART_SESSION_ID] = self.cart
+        # mark the session as "modified", so Django will save it
+        self.session.modified = True
+
+    def remove(self, product):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        # get the product objects and add them to the cart
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]["product"] = product
+        for item in cart.values():
+            item["price"] = Decimal(item["price"])
+            item["total_price"] = Decimal(item["price"]) * item["quantity"]
+            yield item
