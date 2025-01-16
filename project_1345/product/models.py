399fba8e0d13c34f861ba3816b9f377e182f962b
lexxai <lexxai@gmail.com>
2025-01-16 19:22:47 +0200

    added description, owner

--- project_1345/product/models.py
+++ project_1345/product/models.py
@@ -6,6 +6,8 @@ from category.models import Category
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=255)
+    description = models.TextField()
+    owner = models.CharField(max_length=255, null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     property = models.JSONField()
