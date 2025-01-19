d4c102518ffa7fe06be0c0d75c84f7e473ccfddc
lexxai <lexxai@gmail.com>
2025-01-19 05:25:25 +0200

    build

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
-python manage.py runserver --insecure --noreload
+#python manage.py runserver --insecure --noreload
 
 popd > /dev/null
 
