e2be3cf8523372d6ff221256ac11b508feba99a4
lexxai <lexxai@gmail.com>
2025-01-19 05:29:28 +0200

    run  --host 0.0.0.0

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 pushd project_1345
 
-python manage.py runserver --insecure --noreload
+python manage.py runserver --insecure --noreload --port 8000 --host 0.0.0.0
 
 popd > /dev/null
 
