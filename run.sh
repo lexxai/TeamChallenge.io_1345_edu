ddc00daee4258f9402c272617ddffb7513e97c37
lexxai <lexxai@gmail.com>
2025-01-19 05:24:52 +0200

    run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+pushd project_1345
+
+python manage.py runserver --insecure --noreload
+
+popd > /dev/null
+
+#python -m gunicorn --chdir ./project_1345 project_1345.wsgi:application
