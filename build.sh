af801ef6f90bde52567abcab83f31db367a5aac4
lexxai <lexxai@gmail.com>
2025-01-19 05:22:26 +0200

    build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install --upgrade pip
+pip install --upgrade pip -q  --no-input
 pip install -r requirements.txt -q  --no-input
-pip install gunicorn --no-input
+pip install gunicorn -q --no-input
 
 #poetry install -n
 #poetry install --with deploy -n
@@ -16,4 +16,9 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+python manage.py runserver --insecure --noreload
+
 popd > /dev/null
+
+#python -m gunicorn --chdir ./project_1345 project_1345.wsgi:application
