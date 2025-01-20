a634404897189d623a9847e828915bf8215668ae
lexxai <lexxai@gmail.com>
2025-01-20 02:42:04 +0200

    ProductListCreateView Mixin

--- project_1345/product/api_views.py
+++ project_1345/product/api_views.py
@@ -2,9 +2,10 @@ import json
 
 from django_filters import FilterSet, CharFilter
 from rest_framework.exceptions import ValidationError
-from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, GenericAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
+from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from rest_framework.pagination import LimitOffsetPagination
 
 from .serializers import ProductSerializer
@@ -12,8 +13,8 @@ from .models import Product
 
 
 class ProductsPagination(LimitOffsetPagination):
-    default_limit = 10
-    max_limit = 100
+    default_limit = 3
+    max_limit = 25
 
 
 class ProductFilter(FilterSet):
@@ -56,3 +57,26 @@ class ProductCreate(CreateAPIView):
         except ValueError:
             raise ValidationError({"price": "Invalid price format"})
         return super().create(request, *args, **kwargs)
+
+
+class ProductListCreateView(GenericAPIView, ListModelMixin, CreateModelMixin):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+    filterset_class = ProductFilter
+    search_fields = ("name", "description")
+    pagination_class = ProductsPagination
+
+    def get(self, request, *args, **kwargs):
+        """Handle GET request (list)"""
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        """Handle POST request (create)"""
+        try:
+            price = request.data.get("price")
+            if price is not None and float(price) <= 0.0:
+                raise ValidationError({"price": "Price must be greater than 0"})
+        except ValueError:
+            raise ValidationError({"price": "Invalid price format"})
+        return self.create(request, *args, **kwargs)
--- project_1345/product/urls.py
+++ project_1345/product/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .api_views import ProductList, ProductCreate
+from .api_views import ProductList, ProductCreate, ProductListCreateView
 
 urlpatterns = [
-    path("", ProductList.as_view()),
+    path("", ProductListCreateView.as_view()),
+    path("get", ProductList.as_view()),
     path("new/", ProductCreate.as_view()),
 ]
