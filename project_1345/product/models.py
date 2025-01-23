6caec4c393ba82d58aad462f7d7286dbb4a4291f
lexxai <lexxai@gmail.com>
2025-01-23 02:35:07 +0200

    added related_name="images"

--- project_1345/product/models.py
+++ project_1345/product/models.py
@@ -120,7 +120,9 @@ def generate_upload_to(instance, filename):
 
 
 class ProductImage(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="images"
+    )
     image = models.ImageField(upload_to=generate_upload_to)
 
     def __str__(self):
