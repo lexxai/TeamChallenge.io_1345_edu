d0b219ab623767e7e7f2e1383a72c0f5267250e7
lexxai <lexxai@gmail.com>
2025-01-23 07:23:20 +0200

    tune signals for delete old images when new version of file uploaded

--- project_1345/product/signals.py
+++ project_1345/product/signals.py
@@ -1,10 +1,35 @@
 from pathlib import Path
 
 from django.conf import settings
-from django.db.models.signals import post_delete
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models.signals import post_delete, post_save, pre_save
 from django.dispatch import receiver
 
 from product.models import ProductImage
+
+
+@receiver(pre_save, sender=ProductImage)
+def cache_old_image(sender, instance, **kwargs):
+    if instance.pk:
+        try:
+            instance._old_image = sender.objects.get(pk=instance.pk).image
+        except sender.DoesNotExist:
+            instance._old_image = None
+
+
+@receiver(post_save, sender=ProductImage)
+def update_image_file(sender, instance, created, **kwargs):
+    if not created:
+        old_image = getattr(instance, "_old_image", None)
+        new_image = instance.image
+        # print(old_image, new_image)
+        if old_image and old_image != new_image:
+            try:
+                old_image_path = Path(old_image.path)
+                if old_image_path.exists():
+                    old_image_path.unlink()  # Delete the old file
+            except ValueError:
+                pass  # Handle invalid paths
 
 
 @receiver(post_delete, sender=ProductImage)
@@ -19,10 +44,3 @@ def delete_image_file(sender, instance, **kwargs):
         except ValueError:
             # `instance.image.path` may raise a ValueError if the file doesn't exist
             ...
-
-        # Fallback: Construct the path from MEDIA_ROOT and relative path
-        relative_path = instance.image.name  # e.g., "product_images/image.jpg"
-        if relative_path:  # Ensure relative path is not empty
-            full_path = Path(settings.MEDIA_ROOT) / relative_path
-            if full_path.exists():
-                full_path.unlink()
