f81d555ed39751557d54d256262c1a0437170110
lexxai <lexxai@gmail.com>
2025-02-26 12:35:18 +0200

    Languages admin

--- project_1345/language/admin.py
+++ project_1345/language/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from language.models import Language
+
+
 # Register your models here.
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "code")
+    search_fields = ("name", "code")
+    list_filter = ("is_active",)
+
+
+admin.site.register(Language, LanguageAdmin)
